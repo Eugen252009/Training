376abd6e298ec84b5a56aec2c9a4e1681509370a
Eugen <eugen252009@gmail.com>
2022-10-04 16:55:01 +0200

    fundament for new Projects

--- TO-DO list/app.js
+++ TO-DO list/app.js
@@ -3,9 +3,12 @@ const app= express();
 
 app.get("/",function(req,res){
 res.send("Hello");
+res.sendFile(__dirname+"/index.html")
 });
 
 
 app.listen(function(){
 console.log("Server started on Port 3000");
 });
+
+
